ea8d7c37600a9b5e563b971a2a2152d2d564a645
eron2023 <erondauti880@gmail.com>
2025-04-18 15:20:46 +0200

    Krijova AutoMapper

---
+++ ParkingSeleniumProject/Core/AutoMapper/AutoMapperConfigProfile.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ParkingSeleniumProject.Core.AutoMapper
+{
+    public class AutoMapperConfigProfile : Profile
+    {
+        public AutoMapperConfigProfile()
+        {
+
+            CreateMap<ReservationDTO, Reservation>();
+            CreateMap<Reservation, ReservationDTO>();
+
+            CreateMap<ParkingReservationManagerDTO, ParkingReservationManager>();
+            CreateMap<ParkingReservationManager, ParkingReservationManagerDTO>();
+
+            CreateMap<ParkingSpotDTO, ParkingSpot>();
+            CreateMap<ParkingSpot, ParkingSpotDTO>();
+
+            CreateMap<PaymentDto, Payment>();
+            CreateMap<Payment, PaymentDto>();
+
+            CreateMap<PaymentMethodDto, PaymentMethod>();
+            CreateMap<PaymentMethod, PaymentMethodDto>();
+
+            CreateMap<InvoiceDto, Invoice>();
+            CreateMap<Invoice, InvoiceDto>();
+
+            CreateMap<ParkingSpaceDto, ParkingSpace>();
+            CreateMap<ParkingSpace, ParkingSpaceDto>();
+
+            CreateMap<ParkingSpaceManagerDto, ParkingSpaceManager>();
+            CreateMap<ParkingSpaceManager, ParkingSpaceManagerDto>();
+
+            CreateMap<AvailabilityMonitorDto, AvailabilityMonitor>();
+            CreateMap<AvailabilityMonitor, AvailabilityMonitorDto>();
+
+
+        }
+    }
