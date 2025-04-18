69432a16fd96ad874c8318935c7f99fbc646d3f5
Leart Bajrami <73801402+Leart-C@users.noreply.github.com>
2025-04-18 14:57:34 +0200

    dbcontext, contstatst

---
+++ ParkingSeleniumProject/Core/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ParkingSeleniumProject.Core.Entities;
+
+namespace ParkingSeleniumProject.Core.DbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+         
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Log> Logs { get; set; }
+
+    }
+}
