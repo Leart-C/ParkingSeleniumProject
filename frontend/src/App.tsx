e5ff88b6d91359cc7e3fcba69be89ea4959315c9
Leart Bajrami <73801402+Leart-C@users.noreply.github.com>
2025-04-18 17:22:28 +0200

    follderi i frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Toaster } from "react-hot-toast";
+
+// import GlobalRouter from "./routes";
+
+const App = () => {
+  return (
+    <div>
+      {/* <GlobalRouter /> */}
+
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
